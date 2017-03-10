da73082968501a7e2b78a8c53a7ecbb6bb344b9b
shsv382 <shsv382@gmail.com>
2017-03-10 22:10:15 +0400

    Added post creating feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+	include SessionsHelper
+	
+	before_action :set_user, only: [:new, :create]
+
 	def index
 		@posts = Post.all
 	end
@@ -7,12 +11,36 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	def create
-		@post = Post.new(post_params)
+		@post = Post.new(ext_post_params)
+		if @post.save
+			flash[:success] = "Успех!"
+			redirect_to @post
+		else
+			flash.now[:error] = "Введите корректные данные!"
+			render 'new'
+		end
 	end
 
 	private
 	def post_params
 		params.require(:post).permit(:title, :content)
+	end
+
+	def ext_post_params
+		ppr = post_params
+		ppr[:user] = current_user
+		ppr
+	end
+
+	def set_user
+		unless signed_in?
+			flash[:error] = "Войдите, чтобы создавать посты!"
+			redirect_to root_path
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
-  	resources :posts
+  	resources :posts, only: :index
   end
+
+  resources :posts
 
   resources :sessions, only: [:new, :create, :destroy]
   get '/signin',		to: 'sessions#new'
